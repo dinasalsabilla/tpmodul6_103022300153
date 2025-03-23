b6cc88f38e525d425cb1b5b60349401b5c077740
dinasalsabilla <dinasalsabilla05@gmail.com>
2025-03-24 02:58:16 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300153/Program.cs
+++ tpmodul6_103022300153/Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace tpmodul6_103022300153
 {
-    internal class Program
+    class SayaTubeVideo
     {
-        static void Main(string[] args)
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
         {
-            Console.WriteLine("Hello, World!");
+            Random rand = new Random();
+            this.id = rand.Next(10000, 999999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play Count: " + this.playCount);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Dina Salsabillla]");
+            video.IncreasePlayCount(10);
+            video.PrintVideoDetails();
         }
     }
 }
